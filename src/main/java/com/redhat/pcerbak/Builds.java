fac1413075bd09878b4f41018793610977085b50
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-03-13 13:21:23 +0100

    added build nvr lookup

--- src/main/java/com/redhat/pcerbak/Builds.java
+++ src/main/java/com/redhat/pcerbak/Builds.java
@@ -3,7 +3,9 @@ package com.redhat.pcerbak;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Objects;
@@ -51,5 +53,49 @@ public class Builds {
             }
         }
         throw new RuntimeException("No successful build found.");
+    }
+
+    public static String getBuildNvr(File build) {
+        try {
+            File buildXml = new File(build.getAbsolutePath() + "/build.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(buildXml);
+            doc.getDocumentElement().normalize();
+
+            Node firstNvr = doc.getElementsByTagName("nvr").item(0);
+            if(firstNvr != null) {
+                return firstNvr
+                        .getChildNodes()
+                        .item(0)
+                        .getNodeValue();
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("Error getting NVR for " + build.getAbsolutePath() + ".");
+        }
+        return "";
+    }
+
+    public static ArrayList<String> getJobNvrs(File job) {
+        ArrayList<String> nvrs = new ArrayList<>();
+        File buildDir = new File(job.getAbsolutePath() + "/builds/");
+        File[] buildsInDir = Arrays
+                .stream(Objects.requireNonNull(buildDir.listFiles()))
+                .filter(File::isDirectory)
+                .toArray(File[]::new);
+
+        for(File build : buildsInDir) {
+            String nvr = "";
+            try {
+                nvr = getBuildNvr(build);
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(!nvr.equals("") && !nvrs.contains(nvr)) {
+                    nvrs.add(nvr);
+                }
+            }
+        }
+        return nvrs;
     }
 }
