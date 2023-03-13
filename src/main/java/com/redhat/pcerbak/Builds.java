9313f9952dfc3fce6bfcde33eee7096a818ee935
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-03-13 14:36:44 +0100

    updated to use method from report-jck

--- src/main/java/com/redhat/pcerbak/Builds.java
+++ src/main/java/com/redhat/pcerbak/Builds.java
@@ -2,8 +2,8 @@ package com.redhat.pcerbak;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import hudson.plugins.report.jck.main.cmdline.JobsRecognition;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,27 +55,6 @@ public class Builds {
         throw new RuntimeException("No successful build found.");
     }
 
-    public static String getBuildNvr(File build) {
-        try {
-            File buildXml = new File(build.getAbsolutePath() + "/build.xml");
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(buildXml);
-            doc.getDocumentElement().normalize();
-
-            Node firstNvr = doc.getElementsByTagName("nvr").item(0);
-            if(firstNvr != null) {
-                return firstNvr
-                        .getChildNodes()
-                        .item(0)
-                        .getNodeValue();
-            }
-        } catch(Exception e) {
-            throw new RuntimeException("Error getting NVR for " + build.getAbsolutePath() + ".");
-        }
-        return "";
-    }
-
     public static ArrayList<String> getJobNvrs(File job) {
         ArrayList<String> nvrs = new ArrayList<>();
         File buildDir = new File(job.getAbsolutePath() + "/builds/");
@@ -85,15 +64,9 @@ public class Builds {
                 .toArray(File[]::new);
 
         for(File build : buildsInDir) {
-            String nvr = "";
-            try {
-                nvr = getBuildNvr(build);
-            } catch(Exception e) {
-                e.printStackTrace();
-            } finally {
-                if(!nvr.equals("") && !nvrs.contains(nvr)) {
-                    nvrs.add(nvr);
-                }
+            String nvr = JobsRecognition.getChangelogsNvr(build);
+            if(nvr != null && !nvrs.contains(nvr)) {
+                nvrs.add(nvr);
             }
         }
         return nvrs;
